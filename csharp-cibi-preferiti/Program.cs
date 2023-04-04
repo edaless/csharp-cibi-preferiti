6fda315e988c066830003cb11fb80c983aa87905
Emanuele D'Alessandro <dalessandroemanuelek@gmail.com>
2023-04-04 15:46:43 +0200

    bonus fatto

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -4,7 +4,7 @@
 //un array con la classifica dei vostri cibi preferiti (minimo 5, massimo 10 elementi).
 //L’array deve essere già inizializzato nel programma,
 //e i vostri cibi preferiti non vanno chiesti all’utente tramite input.
-string[] cibi = { "pane", "pasta", "pomodoro", "grana", "sale", "calamari" };
+string[] cibi = { "pasta", "pomodoro", "grana", "pepe", "sale", "calamari", "insalata" };
 
 //Una volta dichiarato l’array di cibi preferiti, stampate a video le seguenti informazioni:
 //-La lunghezza della classifica
@@ -27,7 +27,32 @@ Console.WriteLine($"il tuo preferito è: {cibi[0]} \n");
 //-Il vostro cibo preferito ma non troppo (ultima posizione della classifica)
 Console.WriteLine($"quello che meno ti piace: {cibi[cibi.Length - 1]} \n");
 
-//Le modalità di consegna sono le solite: repo GitHub, commit e push
+
 //**BONUS**
-//Stampate a video anche il cibo di mezza classifica, cioè che si trova nella posizione **mediana.** Attenzione: gestire anche il caso se aveste una classifica con un numero di elementi pari. In questo caso vanno stampati i 2 elementi in centro alla vostra classifica.
+//Stampate a video anche il cibo di mezza classifica,
+//cioè che si trova nella posizione **mediana.**
+//Attenzione: gestire anche il caso se aveste una classifica con un numero di elementi pari.
+//In questo caso vanno stampati i 2 elementi in centro alla vostra classifica.
 //****
+
+var mediana = cibi.Length / 2;
+
+
+
+
+//se la length è pari
+if (cibi.Length % 2 == 0)
+{
+    Console.Write($"i cibi a metà sono {mediana}: {cibi[mediana-1]} e {mediana+1}: {cibi[mediana]} ");
+}
+//se la length è dispari
+else
+{
+    Console.Write($"il cibo a metà è {mediana+1}: {cibi[mediana]}");
+
+}
+
+
+
+
+Console.WriteLine($"\n\n\n\n\n\n\n\n");
