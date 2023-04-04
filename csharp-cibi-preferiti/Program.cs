63c1b3fcadc5bf56de9a7458fea52b0038c3d516
Emanuele D'Alessandro <dalessandroemanuelek@gmail.com>
2023-04-04 16:05:46 +0200

    \n sostituito con {Environment.NewLine}

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -4,28 +4,28 @@
 //un array con la classifica dei vostri cibi preferiti (minimo 5, massimo 10 elementi).
 //L’array deve essere già inizializzato nel programma,
 //e i vostri cibi preferiti non vanno chiesti all’utente tramite input.
-string[] cibi = { "pasta", "pomodoro", "pinoli", "crepes", "ragù", "amatriciana", "cacio e pepe", "pizzette", "calamari", "insalata" };
+string[] cibi = { "pomodoro", "crepes", "ragù", "amatriciana", "cacio e pepe", "pizzette", "calamari", "insalata" };
 
 //Una volta dichiarato l’array di cibi preferiti, stampate a video le seguenti informazioni:
 //-La lunghezza della classifica
-Console.WriteLine($"ti piacciono {cibi.Length} cibi \n");
+Console.WriteLine($"ti piacciono {cibi.Length} cibi {Environment.NewLine}");
 
 //- La vostra classifica (dunque stampare l’intero array in ordine indicando la posizione in classifica)
-Console.WriteLine($"in ordine sono: \n");
+Console.WriteLine($"in ordine sono: {Environment.NewLine}");
 for (var i = 0; i < cibi.Length; i++)
 {
     Console.WriteLine($"{i+1}: {cibi[i]}");
 
 }
-Console.WriteLine($"\n");
+Console.WriteLine($"{Environment.NewLine}");
 
 //-Il vostro cibo top (prima posizione della classifica)
-Console.WriteLine($"il tuo preferito è: {cibi[0]} \n");
+Console.WriteLine($"il tuo preferito è: {cibi[0]} {Environment.NewLine}"); 
 
 
 
 //-Il vostro cibo preferito ma non troppo (ultima posizione della classifica)
-Console.WriteLine($"quello che meno ti piace: {cibi[cibi.Length - 1]} \n");
+Console.WriteLine($"quello che meno ti piace: {cibi[cibi.Length - 1]} {Environment.NewLine}");
 
 
 //**BONUS**
@@ -43,8 +43,8 @@ var mediana = cibi.Length / 2;
 //se la length è pari
 if (cibi.Length % 2 == 0)
 {
-    Console.Write($"i cibi a metà sono: \n" +
-        $"{mediana}: {cibi[mediana-1]} \n" +
+    Console.Write($"i cibi a metà sono: {Environment.NewLine}" +
+        $"{mediana}: {cibi[mediana-1]} {Environment.NewLine}" +
         $"{mediana+1}: {cibi[mediana]} ");
 }
 //se la length è dispari
@@ -57,4 +57,4 @@ else
 
 
 
-Console.WriteLine($"\n\n\n\n\n\n\n\n");
+Console.WriteLine($"{Environment.NewLine}{Environment.NewLine}{Environment.NewLine}");
